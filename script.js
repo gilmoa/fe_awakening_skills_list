2c625eee569830dcafd1f5627ef126d3e14f1609
Gilmo <agilmozzi@gmail.com>
2016-05-27 03:45:30 +0200

    Makes classes more obviously clickable

--- script.js
+++ script.js
@@ -79,7 +79,8 @@ var putSkill = function(skill)
 	var tr = $('<tr>').appendTo($('table tbody'));
 	$('<td>').html('<img src="src/icons/' + icon_name + '.png" alt="' + icon_name + '">').appendTo(tr);
 	$('<th scope="row">').text(skill.name).appendTo(tr);
-	$('<td id="class" data-toggle="modal" data-target="#classModal">').text(skill.class).appendTo(tr);
+	var classtr = $('<td id="class" data-toggle="modal" data-target="#classModal">').appendTo(tr);
+	$('<a class="label label-default">').text(skill.class).appendTo(classtr);
 	$('<td class="text-right">').text(skill.level).appendTo(tr);
 	$('<td>').text(skill.activation).appendTo(tr);
 	$('<td class="not-capital">').text(skill.effect).appendTo(tr);
